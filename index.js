66798685efdf34816e37b87ca48553f535ec9302
Uniquecon <akinwunmiolusegun277@gmail.com>
2025-01-19 22:02:06 +0100

    feat: updated the functionality when a game button is clicked

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ const gameButtonDetails = [
   { id: 2, buttonIcon: "./src/assets/icon-rock.svg", buttonAlt: "ROCK", buttonClass: "game-button-rock" },
 ];
 
-function createButton(mainButtonClass, mainButtonIconClass, buttonFunction, { buttonClass, buttonIcon, buttonAlt }) {
+function createButton(mainButtonClass, mainButtonIconClass, { buttonClass, buttonIcon, buttonAlt }, buttonFunction) {
   return `
-    <button onclick="${buttonFunction}" class="${`game-button ${mainButtonClass} ${buttonClass}`}">
-      <img src=${buttonIcon} loading="lazy" class=${mainButtonIconClass} alt="${`${buttonAlt} ICON`}">
+    <button onclick="${buttonFunction}" class="${`game-button ${mainButtonClass} ${buttonClass ? buttonClass : ""}`}">
+      ${buttonIcon ? `<img src=${buttonIcon} loading="lazy" class=${mainButtonIconClass} alt="${`${buttonAlt} ICON`}">` : ""} 
     </button>
   `;
 }
 
-document.getElementById("main-game").innerHTML = gameButtonDetails.map(({ id, buttonIcon, buttonAlt, buttonClass }) => createButton("game-main-button", "game-main-button-icon", `getButtonDetails(${id})`, { buttonClass, buttonIcon, buttonAlt })).join("");
+document.getElementById("main-game").innerHTML = gameButtonDetails.map(({ id, buttonIcon, buttonAlt, buttonClass }) => createButton("game-main-button", "game-main-button-icon", { buttonClass, buttonIcon, buttonAlt }, `getButtonDetails(${id})`)).join("");
 
 function displayMainGame(determineStyle) {
   document.getElementById("main-game").style.display = determineStyle ? "none" : "block";
@@ -20,8 +20,21 @@ function displayMainGame(determineStyle) {
 }
 
 let gameOutcomeButtonDetails = [];
-gameOutcomeButtonDetails = gameOutcomeButtonDetails.concat(Array(2 - gameOutcomeButtonDetails.length).fill({buttonText: "THE HOUSE PICKED", buttonClass: "game-outcome-default-div"}));
-
 function getButtonDetails(id) {
-  
+  const buttonDetails = gameButtonDetails.find((button) => button.id === id);
+  gameOutcomeButtonDetails.push({ ...buttonDetails, buttonText: "YOU PICKED" });
+  gameOutcomeButtonDetails = gameOutcomeButtonDetails.concat(Array(2 - gameOutcomeButtonDetails.length).fill({ buttonText: "THE HOUSE PICKED", buttonClass: "game-outcome-default-div" }));
+  displayGameOutcome(gameOutcomeButtonDetails);
+  displayMainGame(true);
+}
+
+function displayGameOutcome(outcomeArray) {
+  document.getElementById("main-game-outcome").innerHTML += outcomeArray.map(({ buttonText, buttonClass, buttonIcon, buttonAlt }) => {
+    return `
+      <div class="game-outcome-div">
+        <p class="game-outcome-text">${buttonText}</p>
+        ${createButton("game-outcome-button", "game-outcome-button-icon", { buttonClass, buttonIcon, buttonAlt })}
+      </div>
+    `;
+  });
 }
